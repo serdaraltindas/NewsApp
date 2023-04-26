f3a5bfa58b574a40ffce3d78b2cb21d5d95d2858
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-04-26 14:26:48 +0300

    StoryBoard Design

--- NewsApp/NewsApp/Controller/ViewController.swift
+++ NewsApp/NewsApp/Controller/ViewController.swift
@@ -8,6 +8,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
