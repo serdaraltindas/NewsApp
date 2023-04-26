22ffca21a20c7c971a7a28da1cd47ca358604069
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-04-26 15:21:50 +0300

    TableView

--- NewsApp/NewsApp/Controller/ViewController.swift
+++ NewsApp/NewsApp/Controller/ViewController.swift
@@ -6,16 +6,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        tableView.dataSource = self
+        tableView.delegate  = self
     }
-
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! NewsTableViewCell
+        cell.titleLabel.text = "TEST"
+        cell.storyLabel.text = "TEST"
+        return cell
+    }
 }
 
