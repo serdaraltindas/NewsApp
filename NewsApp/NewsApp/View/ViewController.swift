591e0595fde9b40f74d4e4629d2d0e9c96916a78
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-04-26 16:36:10 +0300

    DispatchQueue.main.async

--- NewsApp/NewsApp/View/ViewController.swift
+++ NewsApp/NewsApp/View/ViewController.swift
@@ -10,27 +10,37 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     @IBOutlet weak var tableView: UITableView!
     
+    private var newsTableViewModel : NewsTableViewModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.dataSource = self
         tableView.delegate  = self
         
+        veriAl()
+    }
+    
+    func veriAl() {
         let url = URL(string: "https://raw.githubusercontent.com/atilsamancioglu/BTK-iOSDataSet/master/dataset.json")
         WebService().haberleriIndir(url: url!) { (haberler) in
             if let haberler = haberler {
-                print(haberler)
+                self.newsTableViewModel = NewsTableViewModel(newsList: haberler)
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
             }
         }
     }
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return newsTableViewModel == nil ? 0 : self.newsTableViewModel.numberOfRowsInSection()
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! NewsTableViewCell
-        cell.titleLabel.text = "TEST"
-        cell.storyLabel.text = "TEST"
+        let newsViewModel = self.newsTableViewModel.newsAtIndexPath(indexPath.row)
+        cell.titleLabel.text = newsViewModel.title
+        cell.storyLabel.text = newsViewModel.story
         return cell
     }
 }
