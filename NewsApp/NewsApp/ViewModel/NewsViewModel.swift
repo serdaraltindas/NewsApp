315650a9bc6b0eb15df9b6cb29e826513f2f8250
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-04-26 16:38:49 +0300

    extension

--- NewsApp/NewsApp/ViewModel/NewsViewModel.swift
+++ NewsApp/NewsApp/ViewModel/NewsViewModel.swift
@@ -11,17 +11,26 @@ struct NewsTableViewModel {
     
     let newsList : [News]
     
+}
+
+extension NewsTableViewModel {
+    
     func numberOfRowsInSection() -> Int {
         return self.newsList.count
     }
-    
     func newsAtIndexPath(_ index: Int) -> NewsViewModel {
         let news = self.newsList[index]
         return NewsViewModel(news: news)
     }
 }
+
 struct NewsViewModel {
+    
     let news : News
+    
+}
+
+extension NewsViewModel {
     
     var title : String {
         return self.news.title
@@ -29,5 +38,4 @@ struct NewsViewModel {
     var story : String {
         return self.news.story
     }
-    
 }
