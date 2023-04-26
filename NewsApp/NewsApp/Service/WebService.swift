8c3c3a46ce8bfbe83351491253348d423413909f
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-04-26 15:38:52 +0300

    webService

---
+++ NewsApp/NewsApp/Service/WebService.swift
@@ -0,0 +1,16 @@
+//
+//  WebService.swift
+//  NewsApp
+//
+//  Created by Serdar Altındaş on 26.04.2023.
+//
+
+import Foundation
+
+class WebService {
+    // escaping -> haberleri indirdikten sonra ne yapacağız, çekeceğiz o yüzden ascyn çalışacak.
+    func haberleriIndir(url : URL, completion: @escaping ([News]?) -> ()) {
+        
+    }
+    
+}
